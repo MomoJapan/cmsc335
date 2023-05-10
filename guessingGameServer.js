58e0090da33e41417a3fed10e8cdc2590a504413
MomoJapan <112813093+MomoJapan@users.noreply.github.com>
2023-05-10 14:20:38 -0400

    Update guessingGameServer.js

--- guessingGameServer.js
+++ guessingGameServer.js
@@ -23,7 +23,7 @@ app.set("views", path.resolve(__dirname, "templates"));
 app.set("view engine", "ejs");
 app.listen(portNumber);
 console.log(`Web server started and running at http://localhost:${portNumber}`);
-const url = `https://cmsc335-project.onrender.com/`;
+const url = `https://cmsc335-project.onrender.com`;
 const prompt = "Type stop to shutdown the server: ";
 process.stdout.write(prompt);
 process.stdin.on("readable", function () {
